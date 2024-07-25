ea00ece3cb940964bcd758407bd88985010f8fd9
VexScythe <davide.ludovici@yahoo.it>
2024-07-25 15:49:09 +0200

    feat: export constants

---
+++ lib/COSTANTS.ts
@@ -0,0 +1,11 @@
+export const MARQUEE_ICONS = [
+  { src: '/html.svg', alt: 'HTML' },
+  { src: '/css.svg', alt: 'CSS' },
+  { src: '/javascript.svg', alt: 'JavaScript' },
+  { src: '/typescript.svg', alt: 'TypeScript' },
+  { src: '/tailwind.svg', alt: 'TailwindCSS' },
+  { src: '/react.svg', alt: 'React' },
+  { src: '/next.svg', alt: 'Next' },
+  { src: '/mongodb.svg', alt: 'MongoDB' },
+  { src: '/graphql.svg', alt: 'GraphQL' },
+];
