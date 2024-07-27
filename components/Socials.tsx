09257342ddb2d332aafe60472c29a742f345d60b
VexScythe <davide.ludovici@yahoo.it>
2024-07-27 15:28:21 +0200

    feat: create social section component

---
+++ components/Socials.tsx
@@ -0,0 +1,30 @@
+import Link from 'next/link';
+import { FaGithub, FaLinkedinIn, FaDiscord, FaInstagram } from 'react-icons/fa';
+
+const socials = [
+  { icon: <FaGithub />, path: '' },
+  { icon: <FaLinkedinIn />, path: '' },
+  { icon: <FaDiscord />, path: '' },
+  { icon: <FaInstagram />, path: '' },
+];
+
+interface SocialsProp {
+  containerStyles: string;
+  iconStyles: string;
+}
+
+const Socials = ({ containerStyles, iconStyles }: SocialsProp) => {
+  return (
+    <div className={containerStyles}>
+      {socials.map((social, i) => {
+        return (
+          <Link key={i} href={social.path} className={iconStyles}>
+            {social.icon}
+          </Link>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Socials;
