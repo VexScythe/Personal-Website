07adb3a28ab74689a948fb7f959863d3a0b4b897
VexScythe <davide.ludovici@yahoo.it>
2024-07-28 13:20:00 +0200

    feat: finish last homepage section

--- app/page.tsx
+++ app/page.tsx
@@ -2,23 +2,22 @@ import { Button } from '@/components/ui/button';
 import { FiDownload } from 'react-icons/fi';
 import Socials from '@/components/Socials';
 import Photo from '@/components/Photo';
-import Marquee from '@/components/Marquee';
+import MarqueeComp from '@/components/MarqueeComp';
 
 const Home = () => {
   return (
     <section className="h-full">
       <div className="container mx-auto h-full">
-        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-14">
           <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl">Frontend Developer</span>
             <h1 className="h1 mb-6">
-              Hello I'm <br />
+              Hello, I'm <br />
               <span className="text-accent">Davide Ludovici</span>
             </h1>
             <p className="max-w-[500px] mb-9 text-white/80">
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga,
-              quia nostrum cupiditate quae consectetur enim molestias at
-              maiores.
+              Proud to be a self-taught programmer. Always curious to learn new
+              technologies and determined to improve my skills.
             </p>
             <div className="flex flex-col xl:flex-row items-center gap-8">
               <Button
@@ -41,7 +40,26 @@ const Home = () => {
             <Photo />
           </div>
         </div>
-        <Marquee />
+        <div className="pb-14 flex flex-col gap-6">
+          <h3 className="text-xl">Tech I learned so far</h3>
+          <div
+            style={{
+              height: '4px',
+              background:
+                'linear-gradient(to right, #1c1c22, #00ff99, #1c1c22)',
+              margin: '20px 0',
+            }}
+          />
+          <MarqueeComp />
+          <div
+            style={{
+              height: '4px',
+              background:
+                'linear-gradient(to right, #1c1c22, #00ff99, #1c1c22)',
+              margin: '20px 0',
+            }}
+          />
+        </div>
       </div>
     </section>
   );
