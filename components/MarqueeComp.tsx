a1da1ebc1aa1639ae95886aa29225ee19261959c
VexScythe <davide.ludovici@yahoo.it>
2024-07-29 19:31:29 +0200

    fix: marquee delay in animation

--- components/MarqueeComp.tsx
+++ components/MarqueeComp.tsx
@@ -1,5 +1,6 @@
-import Image from 'next/image';
+'use client';
 import Link from 'next/link';
+import { motion } from 'framer-motion';
 import Marquee from 'react-fast-marquee';
 import {
   SiAstro,
@@ -10,6 +11,7 @@ import {
   SiGraphql,
   SiMongodb,
   SiTailwindcss,
+  SiVite,
   SiReact,
   SiNextdotjs,
 } from 'react-icons/si';
@@ -20,6 +22,7 @@ const techIcons = [
   { icon: <SiJavascript />, path: '' },
   { icon: <SiTypescript />, path: '' },
   { icon: <SiTailwindcss />, path: '' },
+  { icon: <SiVite />, path: '' },
   { icon: <SiAstro />, path: '' },
   { icon: <SiReact />, path: '' },
   { icon: <SiNextdotjs />, path: '' },
@@ -29,21 +32,29 @@ const techIcons = [
 
 export default function MarqueeComp() {
   return (
-    <Marquee
-      gradient={true}
-      gradientColor="#1c1c22"
-      pauseOnHover={true}
-      speed={35}
-      style={{ height: '100px', display: 'flex', alignItems: 'center' }}
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 1, duration: 0.4, ease: 'easeIn' },
+      }}
     >
-      {techIcons.map((item, i) => (
-        <div
-          key={i}
-          style={{ padding: '0 30px', fontSize: '6rem', color: 'white' }}
-        >
-          <Link href={item.path}>{item.icon}</Link>
-        </div>
-      ))}
-    </Marquee>
+      <Marquee
+        gradient={true}
+        gradientColor="#1c1c22"
+        pauseOnHover={true}
+        speed={35}
+        style={{ height: '100px', display: 'flex', alignItems: 'center' }}
+      >
+        {techIcons.map((item, i) => (
+          <div
+            key={i}
+            style={{ padding: '0 30px', fontSize: '6rem', color: 'white' }}
+          >
+            <Link href={item.path}>{item.icon}</Link>
+          </div>
+        ))}
+      </Marquee>
+    </motion.div>
   );
 }
