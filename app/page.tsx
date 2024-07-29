aedd762c40848a24fb3fd533ca57749b3161203a
VexScythe <davide.ludovici@yahoo.it>
2024-07-29 23:08:17 +0200

    fix: homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-import { Button } from '@/components/ui/button';
-import { FiDownload } from 'react-icons/fi';
 import Socials from '@/components/Socials';
 import Photo from '@/components/Photo';
 import MarqueeComp from '@/components/MarqueeComp';
@@ -12,22 +10,14 @@ const Home = () => {
           <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl">Frontend Developer</span>
             <h1 className="h1 mb-6">
-              Hello, I'm <br />
+              Vex alias: <br />
               <span className="text-accent">Davide Ludovici</span>
             </h1>
             <p className="max-w-[500px] mb-9 text-white/80">
               Proud to be a self-taught programmer. Always curious to learn new
               technologies and determined to improve my skills.
             </p>
-            <div className="flex flex-col xl:flex-row items-center gap-8">
-              <Button
-                variant="outline"
-                size="lg"
-                className="uppercase flex items-center gap-2"
-              >
-                <span>Download CV</span>
-                <FiDownload className="text-xl " />
-              </Button>
+            <div className="flex flex-col xl:flex-row items-center">
               <div className="mb-8 xl:mb-0 ">
                 <Socials
                   containerStyles="flex gap-6"
@@ -40,7 +30,7 @@ const Home = () => {
             <Photo />
           </div>
         </div>
-        <div className="pb-14 flex flex-col gap-6">
+        <div className="pb-14 flex flex-col gap-6 text-center xl:text-left">
           <h3 className="text-xl">Tech I learned so far</h3>
           <div
             style={{
