96453a7e8d86fa59427028aec612438c367d0cb9
VexScythe <davide.ludovici@yahoo.it>
2024-07-29 23:07:15 +0200

    feat: create card component for project

---
+++ components/Card.tsx
@@ -0,0 +1,72 @@
+'use client';
+import React from 'react';
+import { motion } from 'framer-motion';
+import Link from 'next/link';
+import Image from 'next/image';
+import { Button } from './ui/button';
+import { FaGithub, FaArrowRight } from 'react-icons/fa';
+import { Project } from '../lib/COSTANTS';
+
+const Card = ({
+  image,
+  type,
+  name,
+  description,
+  siteLink,
+  githubLink,
+  techs,
+}: Project) => {
+  return (
+    <>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{
+          opacity: 1,
+          transition: { delay: 1, duration: 0.4, ease: 'easeIn' },
+        }}
+        className="flex items-center justify-center"
+      >
+        <div className="relative flex flex-col w-full max-w-[48rem] border border-transparent hover:border-accent-hover xl:flex-row rounded-xl bg-secondary bg-clip-border text-white/80 shadow-md">
+          <div className="relative m-0 w-full xl:w-2/5 shrink-0 overflow-hidden rounded-xl xl:rounded-r-none xl:rounded-bl-xl rounded-b-none  bg-clip-border text-gray-700">
+            <Image
+              src={image}
+              alt={name}
+              width={500}
+              height={500}
+              className="h-full w-full object-cover"
+            />
+          </div>
+          <div className="p-6">
+            <h6 className="pb-4 block font-sans text-base font-semibold uppercase leading-relaxed tracking-normal text-accent antialiased">
+              {type}
+            </h6>
+            <h4 className="pb-2 block font-sans text-2xl font-semibold leading-snug tracking-normal text-blue-gray-900 capitalize antialiased">
+              {name}
+            </h4>
+            <p className="pb-4 block font-sans text-sm font-normal leading-relaxed text-white/80 antialiased">
+              {description}
+            </p>
+            <div className="flex pb-4 gap-8 text-accent uppercase text-base flex-wrap">
+              {techs.map((tech, i) => {
+                return <p key={i}>{tech}</p>;
+              })}
+            </div>
+            <div className="flex justify-end items-center gap-4 w-full">
+              <Link href={siteLink} target="_blank">
+                <Button variant="outline" className="flex gap-2 items-center">
+                  Visit
+                  <FaArrowRight />
+                </Button>
+              </Link>
+              <Link href={githubLink} target="_blank">
+                <FaGithub className="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500" />
+              </Link>
+            </div>
+          </div>
+        </div>
+      </motion.div>
+    </>
+  );
+};
+
+export default Card;
