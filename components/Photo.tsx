32495f58c8054e49494c45e2d61e3fad95355f59
VexScythe <davide.ludovici@yahoo.it>
2024-07-29 19:28:53 +0200

    fix: image resolution

--- components/Photo.tsx
+++ components/Photo.tsx
@@ -21,7 +21,7 @@ const Photo = () => {
           className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten absolute"
         >
           <Image
-            src="/angryseal.png"
+            src="/AngrySealbig.png"
             priority
             quality={100}
             fill
