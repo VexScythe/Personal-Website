865282ec1ed41bbf38f6424dd5b047735b07b97a
VexScythe <davide.ludovici@yahoo.it>
2024-07-29 23:06:43 +0200

    feat: add project card and type

--- lib/COSTANTS.ts
+++ lib/COSTANTS.ts
@@ -1,3 +1,6 @@
+import ondalternativa from '../public/projectimg/ondalternativa.png';
+import { StaticImageData } from 'next/image';
+
 export const MARQUEE_ICONS = [
   { src: '/html.svg', alt: 'HTML' },
   { src: '/css.svg', alt: 'CSS' },
@@ -26,5 +29,32 @@ export const links = [
   {
     name: 'contact',
     path: '/contact',
+  },
+];
+
+export interface Project {
+  id?: number;
+  image: StaticImageData;
+  type: string;
+  name: string;
+  description: string;
+  siteLink: string;
+  githubLink: string;
+  techs: string[];
+}
+
+export const PROJECT_CARDS: Project[] = [
+  {
+    id: 1,
+    image: ondalternativa,
+    type: 'webzine',
+    name: 'ondalternativa',
+    description: `Ondalternativa is a music webzine that offers articles, news, and
+                  interviews. The site was developed using Astro for static site
+                  generation, Tailwind CSS and WordPress as the CMS, integrated with
+                  GraphQL for efficient data management.`,
+    siteLink: 'https://www.ondalternativa.it',
+    githubLink: 'https://github.com/scerelli/ondalternativa',
+    techs: ['Astro', 'Tailwind', 'GraphQL'],
   },
 ];
