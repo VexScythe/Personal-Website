a2ad76e903f91246a253d488d9be2822624b1390
VexScythe <davide.ludovici@yahoo.it>
2024-07-30 23:57:50 +0200

    fix: page padding

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -4,7 +4,7 @@ import { PROJECT_CARDS } from '@/lib/COSTANTS';
 
 const Projects = () => {
   return (
-    <section className="h-full">
+    <section className="h-full pb-8">
       <div className="container mx-auto h-full">
         <div className="h3 uppercase pb-8">My projects</div>
         <div className="flex flex-col xl:grid xl:grid-cols-2 gap-8">
