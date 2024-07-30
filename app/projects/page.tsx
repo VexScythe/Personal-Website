2e3550a8bac15e68b00ca140b744784c2221e43a
VexScythe <davide.ludovici@yahoo.it>
2024-07-30 17:58:31 +0200

    feat: create modal animation for cards

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,28 +1,19 @@
-import Card from '@/components/Card';
+'use client';
 import React from 'react';
-import { PROJECT_CARDS } from '@/lib/COSTANTS';
+import { useState } from 'react';
+import ProjectsList from './_components/ProjectsList';
+import Modal from './_components/Modal';
+import { Project } from '@/lib/COSTANTS';
 
 const Projects = () => {
+  const [selected, setSelected] = useState<Project | null>(null);
+
   return (
     <section className="h-full">
       <div className="container mx-auto h-full">
         <div className="h3 uppercase pb-8">My projects</div>
-        <div className="flex flex-col xl:grid xl:grid-cols-2 gap-8">
-          {PROJECT_CARDS.map((item) => {
-            return (
-              <Card
-                key={item.id}
-                image={item.image}
-                type={item.type}
-                name={item.name}
-                description={item.description}
-                siteLink={item.siteLink}
-                githubLink={item.githubLink}
-                techs={item.techs}
-              />
-            );
-          })}
-        </div>
+        <ProjectsList setSelected={setSelected} />
+        <Modal selected={selected} setSelected={setSelected} />
       </div>
     </section>
   );
